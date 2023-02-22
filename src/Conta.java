afb1c94ecbc787c96180c4a803a042f9bdf77b0a
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-02-22 16:41:02 -0300

    feat: new getters and setters

--- src/Conta.java
+++ src/Conta.java
@@ -27,24 +27,32 @@ public class Conta {
 		}
 		return false;
 	}
-	
+
 	public double getSaldo() {
 		return this.saldo;
 	}
-	
+
 	public int getNumero() {
 		return this.numero;
 	}
-	
+
 	public void setNumero(int numero) {
 		this.numero = numero;
 	}
-	
+
 	public int getAgencia() {
 		return this.agencia;
 	}
-	
+
 	public void setAgencia(int agencia) {
 		this.agencia = agencia;
+	}
+
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+
+	public Cliente getTitular() {
+		return titular;
 	}
 }
